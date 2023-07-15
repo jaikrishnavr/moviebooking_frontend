ebb6c121cedb3d9a9cc015d80424afca28a362a0
jaikrishnavr <jaikrishnagokul@gmail.com>
2023-07-15 09:10:35 +0530

    gpay add

--- src/Components/Payments/Payments.js
+++ src/Components/Payments/Payments.js
@@ -1,10 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Button, Modal } from 'react-bootstrap';
+import GooglePayButton from "@google-pay/button-react";
 
-const Payments = ({show , setShow, theatresDetail, movieDetails, selectedSeats, confirmBooking , bookingDetails}) => {
-
-  const handleClose = () => setShow(false)
+const Payments = ({ show, setShow, theatresDetail, movieDetails, selectedSeats, confirmBooking, bookingDetails }) => {
+  const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const [buttonColor, setButtonColor] = useState("default");
+  const [buttonSizeMode, setButtonSizeMode] = useState("static");
+  const [buttonWidth, setButtonWidth] = useState(240);
+  const [paymentStatus, setPaymentStatus] = useState(null);
+  const [buttonHeight, setButtonHeight] = useState(40);
+  const [showModal, setShowModal] = useState(false);
+
+  const paymentRequest = {
+    apiVersion: 2,
+    apiVersionMinor: 0,
+    allowedPaymentMethods: [
+      {
+        type: "CARD",
+        parameters: {
+          allowedAuthMethods: ["PAN_ONLY", "CRYPTOGRAM_3DS"],
+          allowedCardNetworks: ["MASTERCARD", "VISA"]
+        },
+        tokenizationSpecification: {
+          type: "PAYMENT_GATEWAY",
+          parameters: {
+            gateway: "example"
+          }
+        }
+      }
+    ],
+    merchantInfo: {
+      merchantId: "12345678901234567890",
+      merchantName: "Demo Merchant"
+    },
+    transactionInfo: {
+      totalPriceStatus: "FINAL",
+      totalPriceLabel: "Total",
+      totalPrice: "100.00",
+      currencyCode: "IND",
+      countryCode: "IN"
+    }
+  };
+
+  const handlePaymentData = (paymentData) => {
+    // Payment is successful, perform necessary actions here
+    console.log("Payment successful:", paymentData);
+    setPaymentStatus('success');
+    setShowModal(true);
+  };
+
+  const disableButton = selectedSeats.length <= 0;
 
   return (
     <>
@@ -15,78 +62,66 @@ const Payments = ({show , setShow, theatresDetail, movieDetails, selectedSeats, 
         <Modal.Body>
           <div className='row '>
             <div className='col my-2'>
-              
               <h3>{movieDetails.name}</h3>
               <small>{movieDetails.language}</small>
-              <br/>
+              <br />
               <small className='fw-bolder'>{theatresDetail.name}</small>
-              <br/>
-              <small className='text-success'> m- ticket</small>
+              <br />
+              <small className='text-success'>m-ticket</small>
             </div>
             <div className='col-5 my-2'>
-              <h5><span>Tickets : </span>{selectedSeats.length}</h5> 
-              <br/>
+              <h5><span>Tickets:</span> {selectedSeats.length}</h5>
+              <br />
             </div>
-            
-            <hr/>
-
+            <hr />
             <div className='row'>
               <div className='col'>
-                <p> Total</p>
-                </div>
-                <div className='col-4'>
-                <p className='fw-bolder'>Rs. {movieDetails.price*selectedSeats.length}</p>
+                <p>Total</p>
               </div>
-                </div>
-
+              <div className='col-4'>
+                <p className='fw-bolder'>Rs. {movieDetails.price * selectedSeats.length}</p>
+              </div>
+            </div>
           </div>
-
-          {
-
-            bookingDetails&&
-
-<div> 
-  {
-    bookingDetails.status === "SUCCESS" ? (
-      <div className='d-flex flex-column justify-content-between align-items-center'>
-
-        <img src={movieDetails.posterUrl} height={100} width={100} /> 
-        <h5>Booking CONFIRMED !</h5>
-        <small>booking Id :</small>
-        <p>  {bookingDetails._id}</p>
-      </div>
-    ): 
-    <div className='d-flex flex-column justify-content-between align-items-center'>
-
-    <img src={movieDetails.posterUrl} height={100} width={100} /> 
-    <h5>Booking FAILED !</h5>
-    <small>Please Retry</small>
-
-  </div>
-  }
-
-
-</div>
-
-          }
-
-
+          {bookingDetails && paymentStatus === 'success' && (
+            <div>
+              {bookingDetails.status === "SUCCESS" ? (
+                <div className='d-flex flex-column justify-content-between align-items-center'>
+                  <img src={movieDetails.posterUrl} height={100} width={100} alt="Movie Poster" />
+                  <h5>Booking CONFIRMED!</h5>
+                  <small>Booking Id:</small>
+                  <p>{bookingDetails._id}</p>
+                </div>
+              ) : (
+                <div className='d-flex flex-column justify-content-between align-items-center'>
+                  <img src={movieDetails.posterUrl} height={100} width={100} alt="Movie Poster" />
+                  <h5>Booking FAILED!</h5>
+                  <small>Please Retry</small>
+                </div>
+              )}
+            </div>
+          )}
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-      { !bookingDetails &&
-          <Button variant="primary" onClick={confirmBooking}>
-            Confirm Payment
-          </Button>
-
-      }
-
+          {!bookingDetails && !disableButton && (
+            <GooglePayButton
+              environment="TEST" // Set your desired environment (TEST or PRODUCTION)
+              buttonColor={buttonColor}
+              buttonType='book'
+              buttonSizeMode={buttonSizeMode}
+              paymentRequest={paymentRequest}
+              onLoadPaymentData={handlePaymentData}
+              style={{ width: buttonWidth, height: buttonHeight }}
+              onClick={confirmBooking}
+            />
+          )}
         </Modal.Footer>
       </Modal>
     </>
-  )
+  );
 }
 
 export default Payments;
