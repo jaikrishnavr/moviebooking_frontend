88c5ddbc7441db353e380313111d167f93b8d7e4
jaikrishanvr <jaikrishnagokul@gmail.com>
2023-09-13 21:04:48 +0530

    footer added

--- src/Pages/Footer/Foorter.js
+++ src/Pages/Footer/Foorter.js
@@ -5,10 +5,10 @@ import { CFooter, CLink } from '@coreui/react';
 
 const Foorter = () => {
   return (
-    <CFooter position='fixed' className='d-flex justify-content-center'>
+    <CFooter position='fixed' className='d-flex justify-content-center' style={{backgroundColor:"black"}} >
    
     <div >
-      <span >Developed by</span>
+      <span className='text-white'>Developed by</span>
       <CLink style={{color:'red'}} href="https://jaikrishnavr.com"> Jai krishna</CLink>
     </div>
   </CFooter>
